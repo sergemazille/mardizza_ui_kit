52733f63193136233a292a92b531f3cecdfe256c
Serge <serge.mazille@gmail.com>
2017-01-04 23:03:48 +0100

    [WIP] Working on dropdown

--- src/script/Form.js
+++ src/script/Form.js
@@ -6,6 +6,37 @@ export class Form {
     }
 
     static dropdown() {
+        let dropdownTriggers = document.querySelectorAll('.dropdown-trigger');
+        dropdownTriggers.forEach(function(button) {
 
+            // trigger event
+            let $dropdownList = button.parentElement.querySelector('.dropdown-list');
+            let dropdownActive = false;
+
+            button.addEventListener("click", function(e) {
+                e.preventDefault();
+                $dropdownList.style.display = ($dropdownList.style.display == "") ? "block" : "";
+                dropdownActive = $dropdownList.style.display == "block";
+
+                // create a clickable `div` to close the dropdown when user clicks outside of the dropdown element
+                if(dropdownActive) {
+                    let $clickable = document.createElement('div');
+                    $clickable.className = "backdrop";
+
+                    let $body = document.querySelector('body');
+                    $body.appendChild($clickable);
+
+                    $clickable.addEventListener("click", function() {
+                        $dropdownList.style.display = "";
+                        dropdownActive = false;
+                        $clickable.removeEventListener("click", this);
+                        this.remove();
+                    });
+                }
+            });
+
+            // choice event
+
+        });
     }
 }
