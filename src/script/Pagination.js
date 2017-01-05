30902bdc59fa6b9086c6d91957b4cc7360431d11
Serge <serge.mazille@gmail.com>
2017-01-05 23:20:09 +0100

    [WIP] Pagination logic

--- src/script/Pagination.js
+++ src/script/Pagination.js
@@ -6,13 +6,18 @@ export class Pagination {
     }
 
     static pagination() {
-        let pagination = document.querySelector('pagination');
+        let pagination = document.querySelector('.pagination');
+        let items = pagination.querySelectorAll('li');
+        let itemsToKeep = [];
 
-        // step 1: remove unnecessary items
+        items.forEach(function(item, i) {
+            // first and last two items
+            if(0 == i || 1 == i || (items.length - 2) == i || (items.length - 1) == i) {
+                itemsToKeep.push(item);
+            }
+        });
 
-        // step 1a: keep first and last two items
-
-
+        console.log(itemsToKeep);
 
         // replace 'ellipsis' class list item content with 3 dots
         let ellipsisItems = document.querySelectorAll('li.ellipsis');
