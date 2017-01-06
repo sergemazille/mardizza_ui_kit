c0953eef4120879012abc50adf570102a626cdb4
Serge Mazille <serge@wizacha.com>
2017-01-06 14:20:09 +0100

    [WIP] Pagination logic

--- src/script/Pagination.js
+++ src/script/Pagination.js
@@ -7,17 +7,70 @@ export class Pagination {
 
     static pagination() {
         let pagination = document.querySelector('.pagination');
+        let prevItem = pagination.querySelector('.prev');
+        let nextItem = pagination.querySelector('.next');
         let items = pagination.querySelectorAll('li');
-        let itemsToKeep = [];
+        let itemsToKeep = new Map();
+        let activeItemIndex;
 
+        // tests to get activeItemIndex right away
+
+
+
+        // store in a Map the items to display
         items.forEach(function(item, i) {
-            // first and last two items
+            // first and last two items are always displayed
             if(0 == i || 1 == i || (items.length - 2) == i || (items.length - 1) == i) {
-                itemsToKeep.push(item);
+                itemsToKeep.set(i, item);
+            }
+
+            // active items, its previous and next items, are displayed as well
+            if(item.classList.contains('active')) {
+                itemsToKeep.set(i-1, items[i-1]);
+                itemsToKeep.set(i, item);
+                itemsToKeep.set(i+1, items[i+1]);
+
+                activeItemIndex = i; // store the information for later use
             }
         });
 
-        console.log(itemsToKeep);
+        /* add appropriate classes : */
+
+        // disable 'prev' button if active page is the first one
+        if(activeItemIndex == 1) {
+            prevItem.classList.add('disabled');
+            items[3].classList.add('show'); // if active page is 1, the third item is displayed
+        }
+
+        // disable 'next' button if active page is the last one
+        if(activeItemIndex == (items.length - 2)) {
+            nextItem.classList.add('disabled');
+            items[(items.length - 4)].classList.add('show');
+        }
+
+        // first ellipsis check
+        if(activeItemIndex >= 4) { items[2].classList.add('ellipsis', 'show'); }
+
+        // last ellipsis check
+        if(activeItemIndex <= (items.length - 5)) { items[(items.length - 3)].classList.add('ellipsis', 'show'); }
+
+        // active item, previous and next ones
+        items[(activeItemIndex - 1)].classList.add('show');
+        items[activeItemIndex].classList.add('show');
+        items[(activeItemIndex + 1)].classList.add('show');
+
+        // prev, next, first and last pages are displayed as well
+        prevItem.classList.add('show');
+        nextItem.classList.add('show');
+        items[1].classList.add('show');
+        items[(items.length - 2)].classList.add('show');
+
+        // hide every other items
+        items.forEach(function(item) {
+            if(! item.classList.contains('show')) {
+                item.classList.add('hidden');
+            }
+        });
 
         // replace 'ellipsis' class list item content with 3 dots
         let ellipsisItems = document.querySelectorAll('li.ellipsis');
