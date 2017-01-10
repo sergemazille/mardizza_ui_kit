f35f9832c88f55294bbd4b887ddbee64589bc41d
Serge Mazille <serge@wizacha.com>
2017-01-10 17:28:37 +0100

    [WIP] Dialog

--- src/script/Dialog.js
+++ src/script/Dialog.js
@@ -14,11 +14,38 @@ export class Dialog {
         this.backdrop = document.createElement('div');
         this.backdrop.className = "backdrop";
         document.body.appendChild(this.backdrop);
+
+        this.createDialog();
     }
 
     backdropEvents() {
         this.backdrop.addEventListener("click", function() {
             this.remove();
         });
+    }
+
+    createDialog() {
+        // divs creation
+        let dialogContainer = document.createElement('div');
+        let dialogHeader = document.createElement('div');
+        let dialogBody = document.createElement('div');
+        let dialogFooter = document.createElement('div');
+
+        // classes assignations
+        dialogContainer.className = "dialog-success";
+        dialogHeader.className = "dialog-header";
+        dialogBody.className = "dialog-body";
+        dialogFooter.className = "dialog-footer";
+
+        // TEST content assignation
+        dialogHeader.innerHTML = "dialog-header";
+        dialogBody.innerHTML = "dialog-body";
+        dialogFooter.innerHTML = "dialog-footer";
+
+        // DOM integration
+        dialogContainer.appendChild(dialogHeader);
+        dialogContainer.appendChild(dialogBody);
+        dialogContainer.appendChild(dialogFooter);
+        this.backdrop.appendChild(dialogContainer);
     }
 }
