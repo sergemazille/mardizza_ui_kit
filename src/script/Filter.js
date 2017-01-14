de0d095706cdcdf8c3339f5c26ffd93c8f1d2cab
Serge <serge.mazille@gmail.com>
2017-01-14 19:11:20 +0100

    Filters refactoring

--- src/script/Filter.js
+++ src/script/Filter.js
@@ -2,90 +2,62 @@ import * as Utils  from './Utils';
 
 export class Filter {
 
-    // launch class methods
     static init() {
-        this.clearOutButton(); // element creation
-        this.removeFilterOnClick(); // event declaration
-        this.removeFilterTagOnClick(); // event declaration
+        this.createClearOutButton(); // element creation (and then event registration)
+        this.removeItemOnClick(); // register events
     }
 
-    static removeFilterOnClick() {
-        Utils.clickWatch(['filter'], function(filter) {
-            Filter.removeFilterAction(filter);
-        });
-    }
-
-    static removeFilterTagOnClick() {
-        Utils.clickWatch(['filter-tag'], function(tag) {
-            Filter.removeFilterTagAction(tag);
-        });
-    }
-
-    static convertTagToFilter(tag) {
-        // get filter parent for newTag data-target attribute
-        let filterTagsContainer = tag.parentElement.id;
-
-        // create a filter with tag's data
-        let newFilter = document.createElement('li');
-        newFilter.className = "filter";
-        newFilter.innerHTML = tag.innerHTML;
-        newFilter.dataset.origin = `#${filterTagsContainer}`;
-        return newFilter;
-    }
-
-    static convertFilterToTag(filter) {
-        // get filter parent for newTag data-target attribute
-        let filtersContainer = filter.parentElement.id;
-
-        // create a tag with filter's data
-        let newTag = document.createElement('li');
-        newTag.className = "filter-tag";
-        newTag.innerHTML = filter.innerHTML;
-        newTag.dataset.target = `#${filtersContainer}`;
-        return newTag;
-    }
-
-    static removeFilterAction(filter) {
-        // select targeted container
-        let filterTagsContainer = document.querySelector(filter.dataset.origin);
-
-        let newTag = Filter.convertFilterToTag(filter);
-        filter.remove();
-
-        // insert newly created tag into filter tags container
-        filterTagsContainer.appendChild(newTag);
-    }
-
-    static removeFilterTagAction(tag) {
-        // select targeted container and last element (for it to remain the last one)
-        let filtersContainer = document.querySelector(tag.dataset.target);
-        let lastElementChild = filtersContainer.lastElementChild; // last element is clear out button
-
-        let newFilter = Filter.convertTagToFilter(tag);
-        tag.remove();
-
-        // insert newly created filter into filter container
-        filtersContainer.insertBefore(newFilter, lastElementChild);
-    }
-
-    static clearOutButton() {
-        let filterContainers = document.querySelectorAll('.filters');
+    static createClearOutButton() {
+        // target every filter block on the page
+        let filterContainers = document.querySelectorAll('.filter');
+        
+        // create a 'clear filter out' button for each one
         [...filterContainers].forEach(function(container) {
-            // create the 'button'
-            let clearFilter = document.createElement('li');
-            clearFilter.classList.add('clear-filters');
-            container.appendChild(clearFilter);
-            clearFilter.addEventListener('click', function() {
-                Filter.hideFilterContainer(container);
+            let clearOutButton = document.createElement('div');
+            clearOutButton.classList.add('filter-clear');
+            container.appendChild(clearOutButton);
+            
+            // register clear out button click event
+            clearOutButton.addEventListener('click', function() {
+                Filter.clearOutFilters(container);
             });
         });
     }
 
-    static hideFilterContainer(container) {
-        // first remove every child element
-        [...container.children].forEach(function(filter) {
-            if(filter.classList.contains('clear-filters')) { return } // doesn't remove clear out button
-            Filter.removeFilterAction(filter);
+    static removeItemOnClick() {
+        Utils.clickWatch(['filter-item', 'tag-item'], function (item) {
+            Filter.removeItemAction(item);
+        });
+    }
+
+    static convertItem(item) {
+        // get filter parent for newTag data-target attribute
+        let itemDataTarget = item.parentElement.id;
+
+        // create a tag with filter's data
+        let newItem = document.createElement('li');
+        newItem.className = item.classList.contains('filter-item') ? 'tag-item' : 'filter-item';
+        newItem.innerHTML = item.innerHTML;
+        newItem.dataset.target = `#${itemDataTarget}`;
+        return newItem;
+    }
+
+    static removeItemAction(item) {
+        // select targeted container
+        let itemContainer = document.querySelector(item.dataset.target);
+
+        let newItem = Filter.convertItem(item);
+        item.remove();
+
+        // insert newly created tag into filter tags container
+        itemContainer.appendChild(newItem);
+    }
+
+    static clearOutFilters(container) {
+        // get filter items
+        let filterItems = container.querySelectorAll('.filter-item');
+        [...filterItems].forEach(function(filter) {
+            Filter.removeItemAction(filter);
         });
     }
 }
